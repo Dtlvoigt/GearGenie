1b91fa80bef0c1377b8a7fb0813a99d9571f3e29
Dylan Voigt <dtlvoigt@gmail.com>
2024-08-12 22:37:12 -0700

    Added a table for tracking which shops a campaign has access to

--- GearGenie/Models/Campaign.cs
+++ GearGenie/Models/Campaign.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace GearGenie.Models
 {
@@ -6,5 +7,10 @@ namespace GearGenie.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campaign name is required")]
+        public string? Name { get; set; }
+        public int OwnerId { get; set; }
+        public string? Admins { get; set; } //option to allow multiple users to manage a campaign
     }
 }
---
+++ GearGenie/Models/CampaignShop.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GearGenie.Models
+{
+    public class CampaignShop
+    {
+        [Key]
+        public required Campaign Campaign { get; set; }
+        [Key]
+        public required Shop Shop { get; set; }
+    }
+}
