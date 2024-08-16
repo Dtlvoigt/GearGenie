5e4210b569640bcf8a318d6170650c0fce07a16f
Dylan Voigt <dtlvoigt@gmail.com>
2024-08-15 23:44:32 -0700

    Added enums to the Item class for certain properties

--- GearGenie/Models/Item.cs
+++ GearGenie/Models/Item.cs
@@ -2,14 +2,36 @@
 
 namespace GearGenie.Models
 {
+    public enum EquipmentCategory
+    {
+        Weapon,
+        Armor,
+        Item
+    }
+
+    public enum WeaponCategory
+    {
+
+    }
+
+    public enum RangeCategory
+    {
+        Melee,
+        Ranged
+    }
+
     public class Item
     {
         [Key]
         public int Id { get; set; }
         public required string Name { get; set; }
-        public required string Category { get; set; }
+        public required EquipmentCategory EquipmentCategory { get; set; }
+        public WeaponCategory? WeaponCategory { get; set; }
+        public RangeCategory? RangeCategory { get; set; }
         public string? Description { get; set; }
         public int Weight { get; set; } = 0;
 
     }
+
+    
 }
