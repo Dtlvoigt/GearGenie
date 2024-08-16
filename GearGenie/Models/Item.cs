132cbb6922982a6193241279f41598e9c78e12e8
Dylan Voigt <dtlvoigt@gmail.com>
2024-08-15 23:36:13 -0700

    Began adding Item class properties

--- GearGenie/Models/Item.cs
+++ GearGenie/Models/Item.cs
@@ -6,5 +6,10 @@ namespace GearGenie.Models
     {
         [Key]
         public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public string? Description { get; set; }
+        public int Weight { get; set; } = 0;
+
     }
 }
