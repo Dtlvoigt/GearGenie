bf545657fb1b60e62f24487d68035cf404eb929f
Dylan Voigt <dtlvoigt@gmail.com>
2024-08-16 23:39:49 -0700

    Added seperate classes for keeping track of item properties

---
+++ GearGenie/Models/Equipment/WeaponProperty.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GearGenie.Models.Equipment
+{
+    public class WeaponProperty
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
