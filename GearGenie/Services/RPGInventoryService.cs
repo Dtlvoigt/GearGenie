b22012a08b4e54b90208c1175472b648a40dfd28
Dylan Voigt <dtlvoigt@gmail.com>
2024-08-18 23:04:32 -0700

    Added test for deserializing json file into class objects

--- GearGenie/Services/RPGInventoryService.cs
+++ GearGenie/Services/RPGInventoryService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using GearGenie.Models.EquipmentTypes;
+using Microsoft.IdentityModel.Tokens;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace GearGenie.Services
 {
@@ -14,9 +17,13 @@ namespace GearGenie.Services
         {
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync("https://www.dnd5eapi.co/api/equipment-categories");
+            var categories = new List<EquipmentCategory>();
             if (response.IsSuccessStatusCode)
             {
                 var json = await response.Content.ReadAsStringAsync();
+                JsonNode? node = JsonNode.Parse(json);
+                JsonNode root = node.Root;
+                categories = JsonSerializer.Deserialize<List<EquipmentCategory>>(root["results"]);
             }
         }
     }
