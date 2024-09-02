85120fdd130461ef8d8baebd48323e4cc19c9508
Dylan Voigt <dtlvoigt@gmail.com>
2024-09-01 21:03:53 -0700

    Rearranged properties in file

--- GearGenie/Models/Equipment.cs
+++ GearGenie/Models/Equipment.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace GearGenie.Models
@@ -25,86 +27,50 @@ namespace GearGenie.Models
     {
         [Key]
         public int Id { get; set; }
+
         [JsonPropertyName("name")]
         public required string Name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string? URL { get; set; }
+
         [JsonPropertyName("equipment_category")]
-        public required EquipmentCategory EquipmentCategory { get; set; }
+        public JsonElement EquipmentCategoryElement { get; set; }
+        public string? EquipmentCategory => EquipmentCategoryElement.GetProperty("name").GetString();
+
+        //[JsonPropertyName("desc")]
         public string? Description { get; set; }
+
+        public int MoneyAmount { get; set; }
+
+        public string? MoneyType { get; set; }
+
         [JsonPropertyName("cost")]
-        public int Cost { get; set; } = 0;
+        public Cost? CostElement { get; set; }
+        public float Cost { get; set; } = 0;
+        //public Cost Cost
+        //{
+        //    get => new Cost { quantity = MoneyAmount, unit = MoneyType };
+        //    set
+        //    {
+        //        MoneyAmount = value.quantity;
+        //        MoneyType = value.unit;
+        //    }
+        //}
+
         [JsonPropertyName("weight")]
-        public int Weight { get; set; } = 0;
+        public decimal Weight { get; set; } = 0;
 
 
         //weapon properties
         [JsonPropertyName("weapon_category")]
         public string? WeaponCategory { get; set; }
+
         [JsonPropertyName("weapon_range")]
         public string? WeaponRange { get; set; }
+
         [JsonPropertyName("category_range")]
         public string? RangeCategory { get; set; }
-        public int RangeNormal { get; set; } = 0;
-        public int RangeLong { get; set; } = 0;
-        public int ThrowRangeNormal { get; set; } = 0;
-        public int ThrowRangeLong { get; set; } = 0;
-        //[JsonPropertyName("damage_dice")]
-        //public string? DamageDice { get; set; }
-        //[JsonPropertyName("damage_type")]
-        //public string? DamageType { get; set; }
-        //[JsonPropertyName("special")]
-        public string? Special { get; set; }
-        public ICollection<EquipmentWeaponProperty>? WeaponProperties { get; set; }
-
-
-        //armor properties
-        [JsonPropertyName("armor_category")]
-        public string? ArmorCategory { get; set; }
-        [JsonPropertyName("base")]
-        public int ArmorClass { get; set; } = 0;
-        [JsonPropertyName("dex_bonus")]
-        public bool? DexBonus { get; set; }
-        [JsonPropertyName("str_minimum")]
-        public int StrengthMinimum {  get; set; } = 0;
-        [JsonPropertyName("stealth_disadvantage")]
-        public bool? StealthDisadvantage { get; set; }
-
-
-        //nested json elements
-        [JsonPropertyName("equipment_category")]
-        public JsonElement EquipmentCategoryElement { get; set; }
-        public string? EquipmentCategory => EquipmentCategoryElement.GetProperty("name").GetString();
-
-        [JsonPropertyName("damage")]
-        public JsonElement? DamageElement {  get; set; }
-        public string? DamageDice
-        {
-            get
-            {
-                //set property if json element is present
-                if (DamageElement.HasValue && DamageElement.Value.TryGetProperty("damage_dice", out JsonElement damageDiceElement))
-                {
-                    return damageDiceElement.GetString();
-                }
-
-                //return null if this json element is missing
-                return null;
-            }
-        }
-        public string? DamageType
-        {
-            get
-            {
-                //check nested object for damage type property
-                if (DamageElement.HasValue && 
-                    DamageElement.Value.TryGetProperty("damage_type", out JsonElement damageTypeElement) &&
-                    damageTypeElement.TryGetProperty("name", out JsonElement damageTypeName)) 
-                {
-                    return damageTypeName.GetString();
-                }
-                //return null if this json element is missing
-                return null;
-            }
-        }
 
         [JsonPropertyName("range")]
         public JsonElement? RangeElement { get; set; }
@@ -168,6 +134,62 @@ namespace GearGenie.Models
             }
         }
 
+        [JsonPropertyName("damage")]
+        public JsonElement? DamageElement { get; set; }
+        public string? DamageDice
+        {
+            get
+            {
+                //set property if json element is present
+                if (DamageElement.HasValue && DamageElement.Value.TryGetProperty("damage_dice", out JsonElement damageDiceElement))
+                {
+                    return damageDiceElement.GetString();
+                }
+
+                //return null if this json element is missing
+                return null;
+            }
+        }
+        public string? DamageType
+        {
+            get
+            {
+                //check nested object for damage type property
+                if (DamageElement.HasValue &&
+                    DamageElement.Value.TryGetProperty("damage_type", out JsonElement damageTypeElement) &&
+                    damageTypeElement.TryGetProperty("name", out JsonElement damageTypeName))
+                {
+                    return damageTypeName.GetString();
+                }
+                //return null if this json element is missing
+                return null;
+            }
+        }
+
+        //[JsonPropertyName("special")]
+        public string? Special { get; set; }
+
+        public ICollection<EquipmentWeaponProperty>? WeaponProperties { get; set; }
+
+
+        //armor properties
+        [JsonPropertyName("armor_category")]
+        public string? ArmorCategory { get; set; }
+
+        //[JsonPropertyName("base")]
+        public int ArmorClass { get; set; } = 0;
+
+        [JsonPropertyName("dex_bonus")]
+        public bool? DexBonus { get; set; }
+
+        [JsonPropertyName("str_minimum")]
+        public int StrengthMinimum { get; set; } = 0;
+
+        [JsonPropertyName("stealth_disadvantage")]
+        public bool? StealthDisadvantage { get; set; }
+
+
+        //gear properties
         [JsonPropertyName("gear_category")]
         public JsonElement? GearCategoryElement { get; set; }
         public string? GearCategory
@@ -184,8 +206,5 @@ namespace GearGenie.Models
                 return null;
             }
         }
-        //public string? GearCategory => GearCategoryElement.GetProperty("name").GetString();
     }
-
-    
 }
