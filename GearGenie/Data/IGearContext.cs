35bfe3c5253ff456e081896536fec6aadfc8628c
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-01 23:29:07 -0700

    Added new models to interface

--- GearGenie/Data/IGearContext.cs
+++ GearGenie/Data/IGearContext.cs
@@ -1,6 +1,17 @@
-﻿namespace GearGenie.Data
+﻿using GearGenie.Models.EquipmentModels;
+using GearGenie.Models.InventoryModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace GearGenie.Data
 {
     public interface IGearContext :IDisposable
     {
+        DbSet<Equipment> Equipment { get; set; }
+        DbSet<EquipmentCategory> Categories { get; set; }
+        DbSet<WeaponProperty> WeaponProperties { get; set; }
+        DbSet<EquipmentWeaponProperty> EquipmentWeaponProperties { get; set; }
+        DbSet<PackContent> PackContents { get; set; }
+        DbSet<PlayerCharacter> PlayerCharacters { get; set; }
+        DbSet<PlayerItem> PlayerItems { get; set; }
     }
 }
