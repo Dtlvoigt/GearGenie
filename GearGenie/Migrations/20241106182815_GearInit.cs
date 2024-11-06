fc716ef2e287f69542dc9761964b6e00a599f9de
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-06 10:32:57 -0800

    Created new migrations

---
+++ GearGenie/Migrations/20241106182815_GearInit.cs
@@ -0,0 +1,344 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GearGenie.Migrations
+{
+    /// <inheritdoc />
+    public partial class GearInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Campaigns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerId = table.Column<int>(type: "int", nullable: false),
+                    Admins = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campaigns", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    URL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MagicItem = table.Column<bool>(type: "bit", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cost = table.Column<double>(type: "float", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    WeaponCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WeaponRange = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RangeCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RangeNormal = table.Column<int>(type: "int", nullable: true),
+                    RangeLong = table.Column<int>(type: "int", nullable: true),
+                    ThrowRangeNormal = table.Column<int>(type: "int", nullable: true),
+                    ThrowRangeLong = table.Column<int>(type: "int", nullable: true),
+                    DamageDice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DamageType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TwoHandedDamageDice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TwoHandedDamageType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpecialAttribute = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ArmorCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ArmorClass = table.Column<int>(type: "int", nullable: true),
+                    DexBonus = table.Column<bool>(type: "bit", nullable: true),
+                    MaxDexBonus = table.Column<int>(type: "int", nullable: true),
+                    StrengthMinimum = table.Column<int>(type: "int", nullable: true),
+                    StealthDisadvantage = table.Column<bool>(type: "bit", nullable: true),
+                    GearCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ToolCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VehicleCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Speed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rarity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsVariant = table.Column<bool>(type: "bit", nullable: false),
+                    HasVariant = table.Column<bool>(type: "bit", nullable: false),
+                    ParentEquipmentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Equipment_Equipment_ParentEquipmentId",
+                        column: x => x.ParentEquipmentId,
+                        principalTable: "Equipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerCharacters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CampaignId = table.Column<int>(type: "int", nullable: false),
+                    OwnerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerCharacters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeaponProperties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeaponProperties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackContents",
+                columns: table => new
+                {
+                    PackId = table.Column<int>(type: "int", nullable: false),
+                    ContentId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackContents", x => new { x.PackId, x.ContentId });
+                    table.ForeignKey(
+                        name: "FK_PackContents_Equipment_ContentId",
+                        column: x => x.ContentId,
+                        principalTable: "Equipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PackContents_Equipment_PackId",
+                        column: x => x.PackId,
+                        principalTable: "Equipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerItems",
+                columns: table => new
+                {
+                    PlayerCharacterId = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerItems", x => new { x.ItemId, x.PlayerCharacterId });
+                    table.ForeignKey(
+                        name: "FK_PlayerItems_Equipment_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Equipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlayerItems_PlayerCharacters_PlayerCharacterId",
+                        column: x => x.PlayerCharacterId,
+                        principalTable: "PlayerCharacters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CampaignShops",
+                columns: table => new
+                {
+                    CampaignId = table.Column<int>(type: "int", nullable: false),
+                    ShopId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CampaignShops", x => new { x.CampaignId, x.ShopId });
+                    table.ForeignKey(
+                        name: "FK_CampaignShops_Campaigns_CampaignId",
+                        column: x => x.CampaignId,
+                        principalTable: "Campaigns",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CampaignShops_Shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Shops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShopItems",
+                columns: table => new
+                {
+                    ShopId = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ShopSection = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopItems", x => new { x.ShopId, x.ItemId });
+                    table.ForeignKey(
+                        name: "FK_ShopItems_Equipment_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Equipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShopItems_Shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Shops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EquipmentWeaponProperties",
+                columns: table => new
+                {
+                    EquipmentId = table.Column<int>(type: "int", nullable: false),
+                    WeaponPropertyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EquipmentWeaponProperties", x => new { x.EquipmentId, x.WeaponPropertyId });
+                    table.ForeignKey(
+                        name: "FK_EquipmentWeaponProperties_Equipment_EquipmentId",
+                        column: x => x.EquipmentId,
+                        principalTable: "Equipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EquipmentWeaponProperties_WeaponProperties_WeaponPropertyId",
+                        column: x => x.WeaponPropertyId,
+                        principalTable: "WeaponProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CampaignShops_ShopId",
+                table: "CampaignShops",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipment_Name",
+                table: "Equipment",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipment_ParentEquipmentId",
+                table: "Equipment",
+                column: "ParentEquipmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentWeaponProperties_WeaponPropertyId",
+                table: "EquipmentWeaponProperties",
+                column: "WeaponPropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackContents_ContentId",
+                table: "PackContents",
+                column: "ContentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerItems_PlayerCharacterId",
+                table: "PlayerItems",
+                column: "PlayerCharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopItems_ItemId",
+                table: "ShopItems",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeaponProperties_Name",
+                table: "WeaponProperties",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CampaignShops");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "EquipmentWeaponProperties");
+
+            migrationBuilder.DropTable(
+                name: "PackContents");
+
+            migrationBuilder.DropTable(
+                name: "PlayerItems");
+
+            migrationBuilder.DropTable(
+                name: "ShopItems");
+
+            migrationBuilder.DropTable(
+                name: "Campaigns");
+
+            migrationBuilder.DropTable(
+                name: "WeaponProperties");
+
+            migrationBuilder.DropTable(
+                name: "PlayerCharacters");
+
+            migrationBuilder.DropTable(
+                name: "Equipment");
+
+            migrationBuilder.DropTable(
+                name: "Shops");
+        }
+    }
+}
