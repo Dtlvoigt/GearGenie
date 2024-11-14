7a45519534f98c2a1a15a38a29750bb025d68d41
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-13 18:28:02 -0800

    Added view model for an equipment search page

---
+++ GearGenie/Models/ViewModels/EquipmentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GearGenie.Models.EquipmentModels;
+using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class EquipmentViewModel
+    {
+        public List<Equipment> Equipment { get; set; }
+        public List<PlayerCharacter> PlayerCharacters { get; set; }
+        public List<Shop> Shops { get; set; }
+    }
+}
