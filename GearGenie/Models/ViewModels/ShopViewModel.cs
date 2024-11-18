b276f644bfcff3a43d8d363664f0e052865b78bd
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-17 18:57:42 -0800

    Added a viewmodel for shop pages

---
+++ GearGenie/Models/ViewModels/ShopViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class ShopViewModel
+    {
+        public List<Campaign> Campaign { get; set; }
+        public List<PlayerCharacter> PlayerCharacters { get; set; }
+        Shop Shop { get; set; }
+        public List<Shop> Shops { get; set; }
+    }
+}
