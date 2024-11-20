62daec464d562db22a0220d6be184345c9570e0a
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-19 23:34:27 -0800

    Added viewmodel for single equipment pages

---
+++ GearGenie/Models/ViewModels/Equipment.cs
@@ -0,0 +1,12 @@
+﻿using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class Equipment
+    {
+        public Equipment equipment { get; set; }
+        public List<Campaign> Campaigns { get; set; }
+        public List<PlayerCharacter> PlayerCharacters { get; set; }
+        public List<Shop> Shops { get; set; }
+    }
+}
