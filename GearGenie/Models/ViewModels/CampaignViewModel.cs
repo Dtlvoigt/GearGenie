6fb7ef4d26534df0ae2ae97d8d67bde42bfa0fbc
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-20 19:37:43 -0800

    Added campaign viewmodel

---
+++ GearGenie/Models/ViewModels/CampaignViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class CampaignViewModel
+    {
+        public List<PlayerCharacter> PlayerCharacters { get; set; }
+        public List<Shop> Shops { get; set; }
+    }
+}
