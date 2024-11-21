fe4f8cf1fd24ee43df99091b342b4af27033f92b
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-20 19:37:56 -0800

    Added viewmodel for player inventories

---
+++ GearGenie/Models/ViewModels/PlayerInventoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class PlayerInventoryViewModel
+    {
+        public PlayerCharacter playerCharacter {  get; set; }
+        public Equipment equipment { get; set; }
+    }
+}
