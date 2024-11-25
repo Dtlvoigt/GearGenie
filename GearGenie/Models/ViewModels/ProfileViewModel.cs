b7be1e4659d77de99c5247cb48398923265b0988
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-24 22:06:12 -0800

    Added a viewmodel for viewing items tied to your profile

---
+++ GearGenie/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public List<Campaign> Campaigns { get; set; }
+        public List<PlayerCharacter> PlayerCharacters { get; set; }
+        public List<Shop> Shops { get; set; }
+    }
+}
