42b36cd220119ce9f997eeaab8e1e11a6456b83f
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-25 21:54:44 -0800

    Added a viewmodel for a new shop page

---
+++ GearGenie/Models/ViewModels/NewShopViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GearGenie.Models.EquipmentModels;
+using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class NewShopViewModel
+    {
+        public Shop NewShop { get; set; }
+        public List<Campaign> Campaigns { get; set; }
+        public List<Equipment> Equipment { get; set; }
+        public List<Shop> Shops { get; set; }
+    }
+}
