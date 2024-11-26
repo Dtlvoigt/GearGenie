b6de3fc5e4c9d9c0e3e9dce51098bc1f46addf3b
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-25 21:57:52 -0800

    Added a viewmodel for creating a new playercharacter

---
+++ GearGenie/Models/ViewModels/NewPlayerCharacterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GearGenie.Models.InventoryModels;
+
+namespace GearGenie.Models.ViewModels
+{
+    public class NewPlayerCharacterViewModel
+    {
+        public PlayerCharacter PlayerCharacter { get; set; }
+        public Campaign Campaign { get; set; }
+        public List<PlayerCharacter> PlayerCharacters { get; set; }
+        public List<Campaign> Campaigns { get; set; }
+    }
+}
