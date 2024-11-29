b34e5a0f87244c6470e81d56619d46981832463e
Dylan Voigt <dtlvoigt@gmail.com>
2024-11-28 23:02:52 -0800

    Changed ownerId to string

--- GearGenie/Models/InventoryModels/Campaign.cs
+++ GearGenie/Models/InventoryModels/Campaign.cs
@@ -9,7 +9,7 @@ namespace GearGenie.Models.InventoryModels
         public int Id { get; set; }
         [Required(ErrorMessage = "Campaign name is required")]
         public required string Name { get; set; }
-        public required int OwnerId { get; set; }
+        public required string OwnerId { get; set; }
         public string? Admins { get; set; } //option to allow multiple users to manage a campaign
         public ICollection<CampaignShop>? Shops { get; set; }
     }
